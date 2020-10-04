36576861cdafccb4c5ea6493d302717d1aa55767
szapp <dev.szapp@gmail.com>
2020-10-04 13:33:19 +0200

    Fix floating mid-air and ranged combat

--- Ninja/Stamina/Content/integratedSprint.d
+++ Ninja/Stamina/Content/integratedSprint.d
@@ -95,8 +95,11 @@ func int Ninja_Stamina_Npc_IsRunning(var C_Npc slf) {
  * Breath based sprinting. This function has to be called every single frame
  */
 func void Ninja_Stamina_IntegratedSprint() {
-    const int THRESHOLD_MS     = 1161527296; // 3000.0f
-    const int ACTION_WATERWALK = 4;          // oCAniCtrl_Human.actionMode
+    const int THRESHOLD_MS             = 1161527296; // 3000.0f
+    const int ACTION_WATERWALK         = 4;          // oCAniCtrl_Human.actionMode
+    const int FMODE_FAR                = 5;
+    const int oCNpc__SetWeaponMode2_G1 = 6904416; //0x695A60
+    const int oCNpc__SetWeaponMode2_G2 = 7573120; //0x738E80
 
     var oCNpc her; her = Hlp_GetNpc(hero);
     var oCAniCtrl_Human ai; ai = _^(her.human_ai);
@@ -120,9 +123,23 @@ func void Ninja_Stamina_IntegratedSprint() {
             };
         };
     } else if (Ninja_Stamina_Mdl_OverlayMdsIsActive(hero, Ninja_Stamina_SprintMDS)) {
-        Mdl_RemoveOverlayMds(her, Ninja_Stamina_SprintMDS);
-        if (Npc_HasReadiedRangedWeapon(hero)) {
-            AI_Teleport(hero, "PC_HERO");
+        // Check if mid-air, copied from GothicFreeAim <http://github.com/szapp/GothicFreeAim>
+        if (lf(ai._zCAIPlayer_aboveFloor, mkf(50))) {
+            Mdl_RemoveOverlayMds(her, Ninja_Stamina_SprintMDS);
+
+            // Fix ranged combat by re-initializing weapon mode
+            if (Npc_HasReadiedRangedWeapon(hero)) {
+                var int herPtr; herPtr = _@(her);
+                var int zero;
+                const int call = 0;
+                if (CALL_Begin(call)) {
+                    CALL_IntParam(_@(zero));
+                    CALL__thiscall(_@(herPtr), MEMINT_SwitchG1G2(oCNpc__SetWeaponMode2_G1, oCNpc__SetWeaponMode2_G2));
+                    CALL_IntParam(_@(FMODE_FAR));
+                    CALL__thiscall(_@(herPtr), MEMINT_SwitchG1G2(oCNpc__SetWeaponMode2_G1, oCNpc__SetWeaponMode2_G2));
+                    call = CALL_End();
+                };
+            };
         };
     };
 };
